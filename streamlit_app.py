426723103cdff0cbbf16528b2dd6461f7f67108d
Ponath <111901059+Ponath71@users.noreply.github.com>
2022-08-23 22:27:21 +0530

    Update streamlit_app.py
    Fixed Typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.Header('Breakfast Menu')
 
-streamlit.Text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 
