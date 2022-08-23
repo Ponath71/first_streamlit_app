2c3f4b3866b13894963833f09124c5e04a48939c
Ponath <111901059+Ponath71@users.noreply.github.com>
2022-08-23 22:24:42 +0530

    Update streamlit_app.py
    added new Menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
+streamlit.title('Breakfast Menu')
+
 streamlit.title('Omega 3 & Blueberry Oatmeal')
+
+streamlit.title('Kale, Spinach & Rocket Smoothie')
