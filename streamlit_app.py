250b8fc58c8db9f276401035d887711ccce1375f
Ponath <111901059+Ponath71@users.noreply.github.com>
2022-08-23 22:28:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,4 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.Header('Breakfast Menu')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
 
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-
-streamlit.text('Half-Boiled free-Range Egg')
