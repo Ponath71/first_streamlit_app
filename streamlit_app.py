4a5df6d72d5a80a7698ad89ee38b32a54dc80182
Ponath <111901059+Ponath71@users.noreply.github.com>
2022-09-01 18:48:37 +0530

    added try else stmt

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -31,21 +31,31 @@ streamlit.dataframe(fruits_to_show)
 # new chapter to request::27aug2022
 # new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-#added 3rd point
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Pleawse select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+    
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+except URLError as e:    
+  streamlit.error()
+#streamlit.write('The user entered ', fruit_choice)
+
+#import requests
+
 #streamlit.text(fruityvice_response)
 
 #streamlit.text(fruityvice_response.json())
 
 
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+
 
 # dont run anything past here while we trouble shoot
 streamlit.stop()
