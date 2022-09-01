ea99de51d5c4c584987784ff2f3c01f69625e8ba
Ponath <111901059+Ponath71@users.noreply.github.com>
2022-09-01 20:11:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,8 @@ except URLError as e:
 #streamlit.header("The fruit list contains:")
 #streamlit.dataframe(my_data_rows)
 
-streamlit.header("The fruit list contains:")
+#streamlit.header("The fruit list contains:")
+streamlit.header("View our Fruit List - Add your Favorites!")
 #snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
